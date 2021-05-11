6fd49b2ca4063a73bea81a444addc2d8a7d92045
Ankita Puri <pr23.ankita@gmail.com>
2021-05-11 15:11:19 +1000

    ufo-level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,30 +4,63 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
 
-console.log(data);
+// console.log(data);
 
-data.forEach(function(ufoSighting) {
-  console.log(ufoSighting);
-});
 
-// Use d3 to append one table row `tr` for each weather report object
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
-data.forEach(function(ufoSighting) {
-  console.log(ufoSighting);
-    var row = tbody.append("tr");
-});
 
 // Step 3:  Use `Object.entries` to console.log each weather report value
  data.forEach(function(ufoSighting) {
-   console.log(ufoSighting);
+  // console.log(ufoSighting);
     var row = tbody.append("tr");
 
 Object.entries(ufoSighting).forEach(function([key, value]) {
-      console.log(key, value);
+      // console.log(key, value);
     var cell = row.append("td");
     cell.text(value);
    });
  });
 
+// Assign the data from `data.js` to a descriptive variable
+//
 
-        
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log('inputValue',inputValue);
+ // console.log('tableData',tableData);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log('filteredData',filteredData);
+  tbody.html("")
+  filteredData.forEach(function(ufoSighting) {
+    // console.log(ufoSighting);
+      var row = tbody.append("tr");
+  
+  Object.entries(ufoSighting).forEach(function([key, value]) {
+        // console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+     });
+   });
+};
+     
